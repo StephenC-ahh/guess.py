c929f6e96a5946946a811c5832d383194f569e1a
StephenC-ahh <stephen.coatsworth@sky.com>
2021-03-19 14:08:15 +0000

    Updated integer range from 20 to 200

--- Guess.py
+++ Guess.py
@@ -6,8 +6,8 @@ guessesTaken = 0
 print('Hello! What is your name?')
 myName = input()
 
-number = random.randint(1, 20)
-print('Hi, ' + myName + ', I am thinking of a number between 1 and 20.')
+number = random.randint(1, 200)
+print('Hi, ' + myName + ', I am thinking of a number between 1 and 200.')
 
 for guessesTaken in range(6):
     print('Take a guess.') 
